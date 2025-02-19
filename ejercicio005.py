6d66af582e5a55c2b7aba452f9a78cffcd0ca3af
angeldor <angelefrenbarriosromero234@gmail.com>
2025-02-19 11:37:55 -0300

    ejercicio 5y6

--- ejercicio005.py
+++ ejercicio005.py
@@ -4,4 +4,8 @@ area_circulo = 0
 pi = math.pi
 
 def hallar_area(radio):
-    
+    radio = float(input("Ingrese el radio del circulo: "))
+    area_circulo = pi * radio ** 2
+    print("El area del circulo es: ", area_circulo)
+
+hallar_area(area_circulo)
---
+++ ejercicio006.py
@@ -0,0 +1,5 @@
+import random
+
+# Generate a random number between 1 and 100
+random_number = random.randint(1, 1000)
+print(f"Random number between 1 and 100: {random_number}")
